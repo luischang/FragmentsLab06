d545de2b19fcc549db2f0e500eef3a252956f3a9
Luis Chang <luis.chang@outlook.com>
2024-09-15 14:17:19 -0500

    RecyclerView and item_musica

---
+++ app/src/main/java/dev/luischang/fragmentslab06/ui/model/SongModel.kt
@@ -0,0 +1,9 @@
+package dev.luischang.fragmentslab06.ui.model
+
+data class SongModel(
+    val id: Int,
+    val image: Int,
+    val songName: String,
+    val album: String,
+    val numberPlay: String,
+    val duration: String)
